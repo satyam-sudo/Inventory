c50f90db7c5b26e121afb0905a7e529cb82ff4fd
satyam-sudo <guptasatyam323@gmail.com>
2025-07-15 01:32:14 +0530

    1

---
+++ Backend/inventory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace inventory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
